8ab8695353272b15480003d51989969968eabdf1
Ashiqur Rahman <joyashikur@gmail.com>
2019-11-27 23:12:52 +0600

    Dadi Ashse

---
+++ src/Dadi.java
@@ -0,0 +1,16 @@
+public class Dadi {
+    private String Khabar;
+    Dadi(String Khabar)
+    {
+        this.Khabar = Khabar;
+    }
+    public void Daat()
+    {
+        System.out.println("DADIR DAAT NAI!");
+    }
+
+    public void KiKhay()
+    {
+        System.out.println(Khabar+" Khay");
+    }
+}
